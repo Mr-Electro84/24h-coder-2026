6937138f8b21c2ec611d71cae0d028ca73e66d60
Lucas Rosenzweig <xeit0w@gmail.com>
2026-04-21 18:41:44 +0200

    Removed things

--- KotaroInugamie/game-launcher.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-tic80 --skip --fs . --cmd="load assets/game.tic & import code src/main_.fnl & run"
--- KotaroInugamie/run.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-START /MIN CMD.EXE /C powershell -ExecutionPolicy Bypass -File ".\compile-game.ps1"
-game-launcher.bat # For launching the game without a cmd window.
-
-pause
